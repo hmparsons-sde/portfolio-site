c8cb19afef7ce75d4e2ebce238708e18cd448f80
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-04-01 10:37:03 -0500

    styling

--- src/javascripts/components/printProjects.js
+++ src/javascripts/components/printProjects.js
@@ -1,18 +1,18 @@
 const printProjects = (project) => {
   let domString = '';
   project.forEach((element) => {
-    domString += `<div class="card mb-3" style="width: 60rem">
+    domString += `<div class="card mb-3 shadow-lg p-4 rounded" style="width: 60rem">
     <div class="row g-0">
-      <div class="col-md-4">
+      <div class="col-lg-6">
         <img class="card-img" src="${element.screenshot}" alt="project screenshot">
       </div>
-      <div class="col-md-8">
+      <div class="col-lg-6">
         <div class="card-body">
           <h5 class="card-title">${element.title}</h5>
           <p class="card-text description"> ${element.overview}</p>
-            <p class="card-text techs"> ${element.tech}</p>
-            <a href="${element.url}" class="btn btn-outline-light">Project</a>
-            <a href="${element.github}" class="btn btn-outline-light">GitHub</a>
+            <p class="card-text technology"> ${element.tech}</p>
+            <a href="${element.url}" class="btn btn-secondary">Netlify</a>
+            <a href="${element.github}" class="btn btn-secondary">GitHub</a>
         </div>
       </div>
     </div>
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,5 +1,6 @@
 import '../styles/main.scss';
 import bioPage from './components/bioPage';
+import contactPage from './components/contactPage';
 import printApp from './components/printApp';
 import printProjects from './components/printProjects';
 import getProjects from './helpers/data/projectData';
@@ -8,6 +9,7 @@ const init = () => {
   printApp();
   bioPage();
   getProjects().then((projects) => printProjects(projects));
+  contactPage();
 };
 
 init();
