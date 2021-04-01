81ef464219662a67675dbf819c705ac384bbaeb9
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-03-31 20:16:25 -0500

    page is building

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,13 @@
 import '../styles/main.scss';
 import bioPage from './components/bioPage';
 import printApp from './components/printApp';
+import printProjects from './components/printProjects';
+import getProjects from './helpers/data/projectData';
 
 const init = () => {
   printApp();
   bioPage();
+  getProjects().then((projects) => printProjects(projects));
 };
 
 init();
