2902333710dd1864d238754c6ae0d18104372953
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-04-01 19:05:40 -0500

    fixed bio

--- src/javascripts/components/bioPage.js
+++ src/javascripts/components/bioPage.js
@@ -1,8 +1,8 @@
 const bioPage = () => {
-  const bio = `
+  const domString = `
   <div class="container align-self-center" id="bio-card">
   </div>`;
-  document.querySelector('#bio-text').innerHTML = bio;
+  document.querySelector('#bio-text').innerHTML = domString;
 };
 
 export default bioPage;
--- src/javascripts/components/printApp.js
+++ src/javascripts/components/printApp.js
@@ -1,34 +1,36 @@
 const printApp = () => {
   document.querySelector('#main').innerHTML = `
-  <div class="sidenav">
+<div class="sidenav">
   <nav class="navbar" id="navbar">
     <div class="container-fluid">
       <ul id="navLinks" class="nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item text-my-own-color"><a href="#bio-page" id="navToBio" class="nav-link">Me</a></li>
+        <li class="nav-item"><a href="#bio-page" id="navToBio" class="nav-link">Me</a></li>
         <li class="nav-item"><a href="#work-page" id="navToWork" class="nav-link">My work</a></li>
         <li class="nav-item"><a href="#contact-page" id="navToContact" class="nav-link">Get in touch</a></li>
       </ul>
     </div>  
   </nav>
 </div>
+<div class="fullPage">
   <div class="card mb-3" id="bio-page">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img id="bio-image" src="src/images/Headshot2.jpg" alt="bio image" class="img-fluid rounded-3 mx-auto d-block">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-      <div id="bio-text"></div>
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img id="bio-image" src="src/images/Headshot2.jpg" alt="bio image" class="img-fluid rounded-3 mx-auto d-block">
+      </div>
+        <div class="col-md-8">
+          <div class="card-body">
+          <div id="bio-text"></div>
+         </div>
+        </div>
       </div>
     </div>
   </div>
 </div>
-</div>
   <div class="fullPage" id="work-page">
-  <div class="d-flex flex-row flex-wrap justify-content-center" id="work-display"></div>
+    <div class="d-flex flex-row flex-wrap justify-content-center" id="work-display"></div>
   </div>
   <div class="fullPage" id="contact-page">
-  <div class="d-flex flex-row flex-wrap justify-content-center" id="contact-display"></div>
+    <div class="d-flex flex-row flex-wrap justify-content-center" id="contact-display"></div>
   </div>`;
 };
 
