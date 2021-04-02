947642ac0a0a2be95abb24da7ebb6ab22586f151
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-04-01 21:43:49 -0500

    tech logos

--- src/javascripts/components/printApp.js
+++ src/javascripts/components/printApp.js
@@ -4,8 +4,9 @@ const printApp = () => {
   <nav class="navbar" id="navbar">
     <div class="container-fluid">
       <ul id="navLinks" class="nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item"><a href="#bio-page" id="navToBio" class="nav-link">Me</a></li>
+        <li class="nav-item"><a href="#bio-page" id="navToBio" class="nav-link">About me</a></li>
         <li class="nav-item"><a href="#work-page" id="navToWork" class="nav-link">My work</a></li>
+        <li class="nav-item"><a href="#tech-page" id="navToTech" class="nav-link">Tech stack</a></li>
         <li class="nav-item"><a href="mailto:holly-parsons@outlook.com" id="email" class="nav-link" target="_blank">Email me</a></li>
         <li class="nav-item"><a href="https://www.linkedin.com/in/hmparsons/" id="linkedin" class="nav-link" target="_blank">LinkedIn</a></li>
         <li class="nav-item"><a href="https://github.com/hmparsons-sde" id="github" class="nav-link" target="_blank">Github</a></li>
@@ -17,7 +18,7 @@ const printApp = () => {
   <div class="card mb-3" id="bio-page">
     <div class="row g-0">
       <div class="col-md-4">
-        <img id="bio-image" src="src/images/Headshot2.jpg" alt="bio image" class="img-fluid rounded-3 mx-auto d-block">
+        <img id="bio-image" src="" alt="bio image" class="img-fluid rounded-3 mx-auto d-block">
       </div>
         <div class="col-md-8">
           <div class="card-body">
@@ -31,8 +32,10 @@ const printApp = () => {
   <div class="fullPage" id="work-page">
     <div class="d-flex flex-row flex-wrap justify-content-center" id="work-display"></div>
   </div>
-  <div class="fullPage" id="contact-page">
-    <div class="d-flex flex-row flex-wrap justify-content-center" id="contact-display"></div>
+  <div class="fullPage" id="tech-page">
+    <div class="d-flex flex-row flex-wrap justify-content-center" id="tech-display">
+    <img id="tech-logos" src="src/images/techpage.png" alt="tech image" class="img-fluid rounded-3 d-block center">
+    </div>
   </div>`;
 };
 
