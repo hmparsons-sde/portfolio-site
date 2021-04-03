9ec6e7ddc5581629ed621705a98c71b87b1a0e96
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-04-02 20:24:24 -0500

    one page flow

--- src/javascripts/components/printApp.js
+++ src/javascripts/components/printApp.js
@@ -1,10 +1,10 @@
 const printApp = () => {
   document.querySelector('#main').innerHTML = `
-<div class="sidenav">
   <nav class="navbar" id="navbar">
     <div class="container-fluid">
       <ul id="navLinks" class="nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item"><a href="#bio-page" id="navToBio" class="nav-link">About me</a></li>
+        <li class="nav-item"><a href="#" id="navToLanding" class="nav-link">HMP</a></li>
+        <li class="nav-item"><a href="#about-me" id="navToBio" class="nav-link">About me</a></li>
         <li class="nav-item"><a href="#work-page" id="navToWork" class="nav-link">My work</a></li>
         <li class="nav-item"><a href="#tech-page" id="navToTech" class="nav-link">Tech stack</a></li>
         <li class="nav-item"><a href="mailto:holly-parsons@outlook.com" id="email" class="nav-link" target="_blank">Email me</a></li>
@@ -13,30 +13,48 @@ const printApp = () => {
       </ul>
     </div>  
   </nav>
-</div>
-<div class="fullPage">
-  <div class="card mb-3" id="bio-page">
-    <div class="row g-0">
-      <div class="col-md-4">
-        <img id="bio-image" src="" alt="bio image" class="img-fluid rounded-3 mx-auto d-block">
+
+<header class="masthead">
+  <div class="container h-100">
+    <div class="row h-100 align-items-center">
+      <div class="col-12 text-center">
+        <h1 class="mb-3">Holly Parsons</h1>
+        <p class="lead">Web Developer | Yoga Instructor | Accessibility Advocate</p>
       </div>
-        <div class="col-md-8">
-          <div class="card-body">
-          <div id="bio-text"></div>
-         </div>
-        </div>
+    </div>
+  </div>
+</header>
+
+<div class="about-me" id="about-me">
+  <div class="container h-100">
+    <div class="row h-100 align-items-center">
+      <div class="col-12 text-center">
       </div>
     </div>
   </div>
 </div>
-  <div class="fullPage" id="work-page">
-    <div class="d-flex flex-row flex-wrap justify-content-center" id="work-display"></div>
-  </div>
-  <div class="fullPage" id="tech-page">
-    <div class="d-flex flex-row flex-wrap justify-content-center" id="tech-display">
-    <img id="tech-logos" src="src/images/techpage.png" alt="tech image" class="img-fluid rounded-3 d-block center">
+
+<div class="work-page" id="work-page">
+  <div class="container h-100">
+    <div class="row h-100 align-items-center">
+      <div class="col-12 text-center">
+      <div class="d-flex flex-row flex-wrap justify-content-center" id="work-display"></div>
+      </div>
     </div>
-  </div>`;
+  </div>
+</div>
+
+<div class="tech-page" id="tech-page">
+  <div class="container h-100">
+    <div class="row h-100 align-items-center">
+      <div class="col-12 text-center">
+      <div class="d-flex flex-row flex-wrap justify-content-center" id="tech-display">
+      <img id="tech-logos" src="https://github.com/hmparsons-sde/portfolio-site/blob/947642ac0a0a2be95abb24da7ebb6ab22586f151/src/images/techpage.png?raw=true" alt="tech image" class="img-fluid rounded-3 d-block center">
+      </div>
+      </div>
+    </div>
+  </div>
+</div>`;
 };
 
 export default printApp;
